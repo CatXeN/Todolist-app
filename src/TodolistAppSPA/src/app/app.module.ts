d619c34c102afe5c4ce5b2317bfcf0cc0691cb4d
Michał Kaczmarowski <michal@movi.ai>
2022-11-21 23:16:12 +0100

    Apply changes from code review

--- src/TodolistAppSPA/src/app/app.module.ts
+++ src/TodolistAppSPA/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
 import { MenuComponent } from './core/menu/menu.component';
 import { CoreModule } from './core/core.module';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
-
-
 
 @NgModule({
   declarations: [
