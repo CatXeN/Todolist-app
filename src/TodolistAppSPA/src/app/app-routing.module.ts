ac50aef5875cb83ef74c0d4cd8bd04640e11aadf
Michał Kaczmarowski <michal@movi.ai>
2022-11-21 23:12:37 +0100

    Created Angular Project

--- src/TodolistAppSPA/src/app/app-routing.module.ts
+++ src/TodolistAppSPA/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/TodolistAppSPA/src/app/app.module.ts
+++ src/TodolistAppSPA/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
+import { MenuComponent } from './core/menu/menu.component';
+import { CoreModule } from './core/core.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
