a1880abf284345ba03b4a603621e3cd6c18915d9
Hubert K <bertasmonster@gmail.com>
2022-11-21 22:13:34 +0100

    Added database

---
+++ src/TodolistAppAPI/TodolistAppAPI/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodolistAppAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<TodolistAppModels.Entities.Task> Tasks { get; set; }   
+    }
+}
