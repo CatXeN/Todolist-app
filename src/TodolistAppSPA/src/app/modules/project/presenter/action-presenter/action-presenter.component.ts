7ebe8e03e54a8c818b7fe5d41e8de6dcf9395be1
Kamil Kot <kamil.kot07@gmail.com>
2022-11-30 23:27:00 +0100

    added modal for adding task

--- src/TodolistAppSPA/src/app/modules/project/presenter/action-presenter/action-presenter.component.ts
+++ src/TodolistAppSPA/src/app/modules/project/presenter/action-presenter/action-presenter.component.ts
@@ -1,4 +1,6 @@
+import { AddTaskModalComponent } from './../../modal/add-task-modal/add-task-modal.component';
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-action-presenter',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./action-presenter.component.scss']
 })
 export class ActionPresenterComponent {
+  constructor(public dialog: MatDialog) {}
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddTaskModalComponent, {
+      width: '500px',
+      // data: {name: this.name, animal: this.animal},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      // this.animal = result;
+    });
+  }
 }
--- src/TodolistAppSPA/src/app/modules/project/project.module.ts
+++ src/TodolistAppSPA/src/app/modules/project/project.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToDoListPresenterComponent } from './presenter/to-do-list-presenter/to-do-list-presenter.component';
 import { ActionPresenterComponent } from './presenter/action-presenter/action-presenter.component';
+import { AddTaskModalComponent } from './modal/add-task-modal/add-task-modal.component';
 
 @NgModule({
   imports: [
@@ -15,7 +16,8 @@ import { ActionPresenterComponent } from './presenter/action-presenter/action-pr
   declarations: [
     ProjectContainerComponent,
     ToDoListPresenterComponent,
-    ActionPresenterComponent
+    ActionPresenterComponent,
+    AddTaskModalComponent
   ]
 })
 export class ProjectModule { }
